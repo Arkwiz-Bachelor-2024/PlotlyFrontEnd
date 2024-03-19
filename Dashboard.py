b095b01f99c10357132b3a613f635587f5a4b173
Ertshus <siverer@stud.ntnu.no>
2024-03-19 18:53:40 +0100

    Added new components to the dashboard.

--- Dashboard.py
+++ Dashboard.py
@@ -2,10 +2,10 @@ from dash import html, dcc, Dash, Input, Output, callback
 import plotly.graph_objects as go
 import dash
 import base64
+from datetime import datetime
 
-app = dash.Dash(__name__, assets_folder='Resources')
+app = Dash(__name__, assets_folder='Resources')
 
-# Keep to remove the default padding and margin.
 app.index_string = '''
 <!DOCTYPE html>
 <html>
@@ -32,40 +32,118 @@ values = [450, 300, 150, 100]
 fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
 fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
 
+
+start_time = datetime.now()
+
 app.layout = html.Div(style={'width': '100%', 'height': '100vh', 'overflow': 'hidden'},
                       children=[
                           # Left Container
-                          html.Div(id='image-container', style={'width': '70%', 'height': '100vh', 'float': 'left', 'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center', 'justifyContent': 'center', 'backgroundColor': 'rgba(43, 53, 59, 1)'}),
+                          html.Div(style={
+                              'width': '70%',
+                              'height': '100vh',
+                              'float': 'left',
+                              'display': 'flex',
+                              'flexDirection': 'column',
+                              'backgroundColor': 'rgba(43, 53, 59, 1)',
+                          },
+                              children=[
+                                  # Container for Size, Accuracy, Time
+                                  html.Div(style={'display': 'flex', 'justifyContent': 'space-evenly', 'color': 'white',
+                                                  'padding': '10px'},
+                                           children=[
+                                               html.Div(id='image-size', children='Size: '),
+                                               html.Div(id='accuracy', children='Accuracy: '),
+                                               html.Div(id='timer', children='Time: 0'),
+                                           ]),
+                                  dcc.Interval(id='update-time', interval=1000, n_intervals=0),  # Timer
+
+                                  # Container for the original and classified images
+                                  html.Div(style={'flexGrow': '1', 'display': 'flex', 'flexDirection': 'row',
+                                                  'alignItems': 'center', 'justifyContent': 'space-evenly'},
+                                           children=[
+                                               html.Div(id='original-image',
+                                                        style={'maxWidth': '100%', 'maxHeight': '30vh',
+                                                               'marginLeft': '5px', 'float': 'left'}),
+                                               html.Div(id='classified-image',
+                                                        style={'maxWidth': '100%', 'maxHeight': '30vh',
+                                                               'marginLeft': '5px', 'float': 'left'}),
+                                           ]),
+                              ]),
 
                           # Right Container
                           html.Div([
-                              html.Img(src=app.get_asset_url('Arkwiz_Logo.png'), style={'width': '60%', 'height': 'auto', 'display': 'block', 'marginLeft': 'auto', 'marginRight': 'auto', 'marginTop': '20px'}),
-
+                              html.Img(src=app.get_asset_url('Arkwiz_Logo.png'),
+                                       style={'width': '60%', 'height': 'auto', 'display': 'block',
+                                              'marginLeft': 'auto', 'marginRight': 'auto', 'marginTop': '20px'}),
+                              html.Div(id='file-path', children='No file selected',
+                                       style={'color': 'white', 'textAlign': 'center', 'margin': 'auto',
+                                              'fontFamily': 'Arial', 'fontWeight': 'bold', 'fontSize': '20px',
+                                              'marginTop': '60px'}),
                               dcc.Upload(
-                                    id='upload-image',
-                                    children=html.Button('Upload Image', id='open-file-dialog-btn', style={'height': '80px', 'width': '200px', 'backgroundColor': 'rgba(18, 88, 103, 1)', 'color': 'rgba(255, 255, 255, 1)', 'fontFamily': 'Arial', 'fontWeight': 'bold', 'fontSize': '24px', 'borderWidth': '1px', 'borderStyle': 'none', 'borderRadius': '10px', 'textAlign': 'center', 'margin': 'auto', 'display': 'block'}),
-                                    style={
-                                        'width': '60%', 'height': '60px', 'lineHeight': '60px', 'borderWidth': '1px', 'borderStyle': 'none', 'borderRadius': '0px', 'textAlign': 'center', 'margin': '110px auto', 'marginBottom': '10px'
-                                    },
+                                  id='upload-image',
+                                  children=html.Button('Upload Image', style={'height': '80px', 'width': '200px',
+                                                                              'backgroundColor': 'rgba(18, 88, 103, 1)',
+                                                                              'color': 'rgba(255, 255, 255, 1)',
+                                                                              'fontFamily': 'Arial',
+                                                                              'fontWeight': 'bold', 'fontSize': '24px',
+                                                                              'borderWidth': '1px',
+                                                                              'borderStyle': 'none',
+                                                                              'borderRadius': '10px',
+                                                                              'textAlign': 'center', 'margin': 'auto',
+                                                                              'display': 'block'}),
+                                  style={'width': '60%', 'height': '60px', 'lineHeight': '60px', 'borderWidth': '1px',
+                                         'borderStyle': 'none', 'borderRadius': '0px', 'textAlign': 'center',
+                                         'margin': '110px auto', 'marginBottom': '10px'},
                                   multiple=False,
                                   accept='.jpg'
                               ),
-
                               html.Div([
                                   dcc.Graph(id='classification-graph', figure=fig),
                               ], style={'marginTop': '20px'}),
-                          ], style={'width': '30%', 'height': '100vh', 'float': 'right', 'display': 'flex', 'flexDirection': 'column', 'justifyContent': 'flex-start', 'backgroundColor': 'rgba(88, 98, 105, 1)'})
+                          ], style={'width': '30%', 'height': '100vh', 'float': 'right', 'display': 'flex',
+                                    'flexDirection': 'column', 'justifyContent': 'flex-start',
+                                    'backgroundColor': 'rgba(88, 98, 105, 1)'})
                       ])
-@callback(Output('image-container', 'children'),
-          Input('upload-image', 'contents'))
 
+
+@callback(
+    [Output('original-image', 'children'), Output('classified-image', 'children')],
+    [Input('upload-image', 'contents')]
+)
 def update_image(contents):
-    if contents is not None:
-        content_type, content_string = contents.split(',')
-        decoded = base64.b64decode(content_string)
-        img_src = f'data:image/jpg;base64,{content_string}'
-        return html.Img(src=img_src, style={'maxWidth': '100%', 'maxHeight': '55vh', 'marginLeft': '5px', 'float': 'left'})
-    return html.H1('Please upload the image you wish to classify!', style={'color': 'white', 'fontFamily': 'Arial'})
+    if contents is None:
+        no_image_message = "Please upload the image you wish to classify!"
+        return html.Div(no_image_message,
+                        style={'color': 'white', 'textAlign': 'center', 'margin': 'auto', 'fontFamily': 'Arial',
+                               'fontWeight': 'bold', 'fontSize': '20px', 'marginTop': '60px'}), html.Div(
+            no_image_message, style={'color': 'white', 'textAlign': 'center', 'margin': 'auto', 'fontFamily': 'Arial',
+                                     'fontWeight': 'bold', 'fontSize': '20px', 'marginTop': '60px'})
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    img_src = f'data:image/jpg;base64,{content_string}'
+    image_element = html.Img(src=img_src, style={'maxWidth': '100%', 'maxHeight': '40vh', 'marginLeft': '5px'})
+    return image_element, image_element
+
+
+@callback(
+    Output('file-path', 'children'),
+    [Input('upload-image', 'filename')]
+)
+def update_file_path(filename):
+    if filename is not None:
+        return filename
+    return 'No file selected'
+
+
+@callback(
+    Output('timer', 'children'),
+    [Input('update-time', 'n_intervals')]
+)
+def update_timer(n):
+    global start_time
+    time_elapsed = datetime.now() - start_time
+    return f"Time: {time_elapsed.seconds}"
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
