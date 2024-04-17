Fixed image resizing and scaling before mask extracting

utils/image_preparation.py
@@ -31,13 +31,14 @@ def load_images_from_folder(folder):
     for filename in img_paths:
         try:
             with Image.open(filename) as img:
-                img = img.convert('RGB')  # Convert to RGB if not already
-                img = tf_image.resize(img, (512, 512))  # Resizing using TensorFlow
-                img = cast(img, float32) / 255.0  # Normalize the image
-                img = Image.fromarray(tf_image.convert_image_dtype(img, tf.uint8).numpy())  # Convert back to PIL Image
+                img = img.copy()
+                img = tf_image.resize(img, (512, 512))
+                img = tf.cast(img, tf.float32) / 255.0
+                img = tf_image.convert_image_dtype(img, "float32")
                 images.append(img)
         except IOError:
             print(f"Failed to load {filename}")
+            
     return images
 
 